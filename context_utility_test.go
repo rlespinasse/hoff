d138137759abf7f30bf00e4647a468694623f060
Romain Lespinasse <romain.lespinasse@gmail.com>
2018-08-18 13:35:14 +0200

    feat(context): add context cmp opts

--- context_utility_test.go
+++ context_utility_test.go
@@ -1,3 +1,9 @@
 package flow
 
+import "github.com/google/go-cmp/cmp"
+
 var emptyContext = NewContext()
+
+var contextEqualOpts = cmp.Comparer(func(x, y contextData) bool {
+	return (x == nil && y == nil) || (x != nil && y != nil && cmp.Equal(x, y))
+})
