e86ee46162431bfd97bacf7f9c5eb08e37530487
Romain Lespinasse <romain.lespinasse@gmail.com>
2018-08-18 17:25:46 +0200

    feat(context): learn to delete data key

--- context.go
+++ context.go
@@ -12,6 +12,10 @@ func (c *Context) Store(key string, value interface{}) {
 	c.data[key] = value
 }
 
+func (c *Context) Delete(key string) {
+	delete(c.data, key)
+}
+
 func (c *Context) Read(key string) (interface{}, error) {
 	value, ok := c.data[key]
 	if ok {
--- context_test.go
+++ context_test.go
@@ -50,3 +50,90 @@ func Test_Context_Read(t *testing.T) {
 		})
 	}
 }
+			}
+		})
+	}
+}
+
+func Test_Context_Read(t *testing.T) {
+	testCases := []struct {
+		name             string
+		givenContextData contextData
+		givenKey         string
+		expectedValue    interface{}
+		expectedError    error
+	}{
+		{
+			name:             "Can read present key",
+			givenContextData: contextData{"key": "value"},
+			givenKey:         "key",
+			expectedValue:    "value",
+		},
+		{
+			name:          "Can't read unknown key",
+			givenKey:      "key",
+			expectedError: errors.New("unknown key: key"),
+		},
+	}
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			c := setupContext(testCase.givenContextData)
+			value, err := c.Read(testCase.givenKey)
+
+			if value != testCase.expectedValue {
+				t.Errorf("value - got: %+v, want: %+v", value, testCase.expectedValue)
+			}
+			if !cmp.Equal(err, testCase.expectedError, errorEqualOpts) {
+				t.Errorf("error - got: %+v, want: %+v", err, testCase.expectedError)
+			}
+		})
+	}
+}
+
+func Test_Context_Delete(t *testing.T) {
+	testCases := []struct {
+		name                string
+		givenContextData    contextData
+		givenKey            string
+		expectedContextData contextData
+	}{
+		{
+			name:                "Can delete a key",
+			givenContextData:    contextData{"key": "value"},
+			givenKey:            "key",
+			expectedContextData: contextData{},
+		},
+		{
+			name: "Can delete a present key without deleting other keys",
+			givenContextData: contextData{
+				"key":         "value",
+				"another_key": "another_value",
+			},
+			givenKey: "another_key",
+			expectedContextData: contextData{
+				"key": "value",
+			},
+		},
+		{
+			name: "Can delete a missing key",
+			givenContextData: contextData{
+				"key": "value",
+			},
+			givenKey: "missing_key",
+			expectedContextData: contextData{
+				"key": "value",
+			},
+		},
+	}
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			c := setupContext(testCase.givenContextData)
+			c.Delete(testCase.givenKey)
+
+			if !cmp.Equal(c.data, testCase.expectedContextData) {
+				t.Errorf("got: %+v, want: %+v", c.data, testCase.expectedContextData)
+			}
+		})
+	}
+}
+
