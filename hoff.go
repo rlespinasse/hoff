4fc1441bba6bc5a950345484cb33f11c20245885
Romain Lespinasse <romain.lespinasse@gmail.com>
2018-10-05 22:08:35 +0200

    docs: fixes conflict with package and variables

--- hoff.go
+++ hoff.go
@@ -3,26 +3,29 @@ Package hoff is a library to define a Node workflow and compute data against it.
 
 Create a node system and activate it:
 
-	nodesystem := system.New()
-	nodesystem.AddNode(some_action_node) // read the input_data in context and create a output_data in context
-	nodesystem.AddNode(decision_node) // check if the input_data is valid with some functionals rules
-	nodesystem.AddNode(another_action_node) // enhance output_data with some functionals tasks
-	nodesystem.ConfigureJoinModeOnNode(another_action_node, joinmode.AND)
-	nodesystem.AddLink(some_action_node, another_action_node)
-	nodesystem.AddLinkOnBranch(decision_node, another_action_node, true)
-	nodesystem.Activate()
+	ns := system.New()
+	ns.AddNode(some_action_node) // read the input_data in context and create a output_data in context
+	ns.AddNode(decision_node) // check if the input_data is valid with some functionals rules
+	ns.AddNode(another_action_node) // enhance output_data with some functionals tasks
+	ns.ConfigureJoinModeOnNode(another_action_node, joinmode.AND)
+	ns.AddLink(some_action_node, another_action_node)
+	ns.AddLinkOnBranch(decision_node, another_action_node, true)
+	errs := ns.Activate()
+	if errs != nil {
+		// error handling
+	}
 
 Create a computation and launch it:
 
-	context := node.NewContextWithoutData()
-	context.Store("input_info", input_info)
-	computation := computation.New(nodesystem, context)
-	err := computation.Compute()
+	cxt := node.NewContextWithoutData()
+	cxt.Store("input_info", input_info)
+	cp := computation.New(nodesystem, context)
+	err := cp.Compute()
 	if err != nil {
 		// error handling
 	}
-	fmt.Printf("computation report: %+v", computation.Report)
-	fmt.Printf("output_data: %+v", computation.Context.Read("output_data"))
+	fmt.Printf("computation report: %+v", cp.Report)
+	fmt.Printf("output_data: %+v", cp.Context.Read("output_data"))
 
 */
 package hoff
